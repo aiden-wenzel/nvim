2847b6948bf84bf761971d4da308a38eba7b4278
aidenwen <wenzel.aiden@outlook.com>
2024-04-23 17:01:26 -0400

    wrote remap.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,1 @@
-require("plugins")
-require("treesitter")
-require("spacing")
-require("telescope")
-
-vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
+require("aidenwen")
---
+++ lua/aidenwen/init.lua
@@ -0,0 +1,1 @@
+require("aidenwen.remap")
