7e157685de510870305cc4d58976b284112c9553
aidenwen <wenzel.aiden@outlook.com>
2024-04-27 00:53:00 -0400

    finally fixed function signatures

--- lua/aidenwen/plugins/lazy.lua
+++ lua/aidenwen/plugins/lazy.lua
@@ -44,6 +44,7 @@ local plugins = {
         "williamboman/mason-lspconfig.nvim",
         "neovim/nvim-lspconfig",
         'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-nvim-lsp-signature-help',
         'hrsh7th/nvim-cmp',
         'L3MON4D3/LuaSnip',
     },
--- lua/aidenwen/plugins/lsp.lua
+++ lua/aidenwen/plugins/lsp.lua
@@ -2,13 +2,33 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities(
     vim.lsp.protocol.make_client_capabilities()
 )
 
-require("lspconfig").lua_ls.setup{
-    capabilities = capabilities,
-}
-require("lspconfig").clangd.setup{
-    capabilities = capabilities
-}
+require("mason").setup({})
+require("mason-lspconfig").setup({
+    ensure_installed = {
+        "lua_ls",
+        "clangd",
+        "pylsp",
+    },
+    handlers = {
+        function(server_name)
+            require("lspconfig")[server_name].setup({
+                capabilities = capabilities
+            })
+        end,
+    }
+})
 
-require("lspconfig").pyright.setup {
-    capabilities = capabilities
-}
+local cmp = require("cmp")
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+        end,
+    },
+
+    sources = cmp.config.sources({
+        {name = "nvim_lsp"},
+        {name = "nvim_lsp_signature_help"}
+    })
+})
