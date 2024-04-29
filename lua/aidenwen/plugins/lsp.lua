28035e15529a03e4bf448068d0312f716aab1d40
aidenwen <wenzel.aiden@outlook.com>
2024-04-29 14:36:26 -0400

    lsp keybindings

--- lua/aidenwen/plugins/lsp.lua
+++ lua/aidenwen/plugins/lsp.lua
@@ -26,10 +26,23 @@ cmp.setup({
             require("luasnip").lsp_expand(args.body)
         end,
     },
-
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['C-p'] = cmp.mapping.select_prev_item(cmp_select),
+        ['C-n'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    }),
     sources = cmp.config.sources({
         {name = "nvim_lsp"},
         {name = "nvim_lsp_signature_help"},
         {name = "luasnip"}
     })
 })
+
---
+++ lua/aidenwen/plugins/neotree.lua
@@ -0,0 +1,19 @@
+require("neo-tree").setup({
+    default_component_configs = {
+        git_status = {
+            symbols = {
+                -- Change type
+                added     = "✚",
+                deleted   = "✖",
+                modified  = "",
+                renamed   = "󰁕",
+                -- Status type
+                untracked = "",
+                ignored   = "",
+                unstaged  = "󰄱",
+                staged    = "",
+                conflict  = "",
+            }
+        }
+    }
+})
