b49b2973ca4cce1919a3f19d4cbb5d2247448614
aidenwen <wenzel.aiden@outlook.com>
2024-04-30 15:35:38 -0400

    enabled symantic highlighting

--- lua/aidenwen/plugins/treesitter.lua
+++ lua/aidenwen/plugins/treesitter.lua
@@ -1,4 +1,7 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "c", "lua", "vim", "python", "cpp",},
+  highlight = {
+      enable = true,
+  },
 }
