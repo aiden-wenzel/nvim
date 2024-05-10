923e3f4710c93b36e2c89f8fba453ba369f9a2b6
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-10 14:59:47 -0400

    vim-tmux keybinds

--- lua/aidenwen/plugins/lazy.lua
+++ lua/aidenwen/plugins/lazy.lua
@@ -31,7 +31,22 @@ local plugins = {
     "mfussenegger/nvim-dap",
     "jay-babu/mason-nvim-dap.nvim",
     { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} },
-    "christoomey/vim-tmux-navigator"
+    {"christoomey/vim-tmux-navigator",
+    cmd = {
+        "TmuxNavigateLeft",
+        "TmuxNavigateDown",
+        "TmuxNavigateUp",
+        "TmuxNavigateRight",
+        "TmuxNavigatePrevious",
+    },
+    keys = {
+        { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+        { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+        { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+        { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+        { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+}
 }
 
 local opts = {}
