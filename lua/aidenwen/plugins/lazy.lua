205f4629945a3486fa1cb7130e1f1c7a122b1c54
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-10 14:50:59 -0400

    tmux vim plugin

--- lua/aidenwen/plugins/lazy.lua
+++ lua/aidenwen/plugins/lazy.lua
@@ -31,6 +31,7 @@ local plugins = {
     "mfussenegger/nvim-dap",
     "jay-babu/mason-nvim-dap.nvim",
     { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} },
+    "christoomey/vim-tmux-navigator"
 }
 
 local opts = {}
