a1ae10422e4a41531477a708a8506cbe05ff1389
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-14 20:38:28 -0400

    new keybinds

--- lua/aidenwen/plugins/debugger.lua
+++ lua/aidenwen/plugins/debugger.lua
@@ -2,14 +2,19 @@ local dap = require("dap")
 local dapui = require("dapui")
 dapui.setup()
 
-vim.keymap.set("n", "<Leader>sc", dap.continue)
-vim.keymap.set("n", "<Leader>sv", dap.step_over)
+vim.keymap.set("n", "<Leader><F5>", dap.continue)
+vim.keymap.set("n", "<Leader>so", dap.step_over)
 vim.keymap.set("n", "<Leader>si", dap.step_into)
-vim.keymap.set("n", "<Leader>so", dap.step_out)
+vim.keymap.set("n", "<Leader>sv", dap.step_out)
 
 vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint)
 
+vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
+    require('dap.ui.widgets').hover()
+end)
+
 vim.keymap.set("n", "<Leader>di", dapui.toggle)
+vim.keymap.set("n", "<Leader>dr", dap.repl.open)
 
 dap.adapters.gdb = {
   type = "executable",
