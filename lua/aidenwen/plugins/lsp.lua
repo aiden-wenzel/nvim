0d30cd99d63db9d627784085041d033e77a6f2e9
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-15 14:56:02 -0400

    moved cmp stuff to lsp

--- lua/aidenwen/plugins/lsp.lua
+++ lua/aidenwen/plugins/lsp.lua
@@ -1,7 +1,15 @@
-return { 
+return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
-		"williamboman/mason.nvim",
+		'neovim/nvim-lspconfig',
+		"hrsh7th/nvim-cmp",
+		'hrsh7th/cmp-nvim-lsp',
+		'hrsh7th/cmp-buffer',
+		'hrsh7th/cmp-path',
+		'hrsh7th/cmp-cmdline',
+		'L3MON4D3/LuaSnip',
+		'saadparwaiz1/cmp_luasnip',
+ 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
 	},
 	config = function()
@@ -43,8 +51,31 @@ return {
 			}
 		})
 
+		local cmp = require("cmp")
+		local cmp_select = { behavior = cmp.SelectBehavior.Select }
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			mappings = cmp.mapping.preset.insert({
+				['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+				['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+				['<C-y>'] = cmp.mapping.confirm({ select = true }),
+				["<C-Space>"] = cmp.mapping.complete(),
+			}),
+			sources = cmp.config.sources({
+				{ name = 'nvim_lsp' },
+               			{ name = 'luasnip' },
+				{ name = 'buffer' },
+			}),
+		})
+
 
 	end
 }
-
-
