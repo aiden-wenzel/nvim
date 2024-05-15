6dfecc06c91b873794aec6017a1945f8b3c19e9d
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-15 14:42:27 -0400

    added luasnip and set snippet engine

--- lua/aidenwen/plugins/cmp.lua
+++ lua/aidenwen/plugins/cmp.lua
@@ -6,5 +6,21 @@ return {
 		'hrsh7th/cmp-buffer',
 		'hrsh7th/cmp-path',
 		'hrsh7th/cmp-cmdline',
-	}
+		'L3MON4D3/LuaSnip',
+		'saadparwaiz1/cmp_luasnip'
+	},
+
+	config = function()
+		local cmp = require("cmp")
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			window = {},
+			mappings = {},
+			sources = {},
+		})
+	end
 }
