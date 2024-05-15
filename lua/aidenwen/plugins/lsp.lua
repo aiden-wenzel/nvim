29b88e041b0ab3f3d6cabe1b8ebd0713f7c08fa9
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-15 14:24:30 -0400

    language server is now online

--- lua/aidenwen/plugins/lsp.lua
+++ lua/aidenwen/plugins/lsp.lua
@@ -1,11 +1,19 @@
-return {
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
+return { 
 	"neovim/nvim-lspconfig",
-
-	config = function() 
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+	--	"hrsh7th/cmp-nvim-lsp",
+	--	"hrsh7th/cmp-buffer",
+	--	"hrsh7th/cmp-path",
+	--	"hrsh7th/cmp-cmdline",
+	--	"hrsh7th/nvim-cmp",
+	--	"L3MON4D3/LuaSnip",
+	--	"saadparwaiz1/cmp_luasnip",
+	--	"j-hui/fidget.nvim",
+	},
+	config = function()
 		require("mason").setup({
-
 			ui = {
 				icons = {
 					package_installed = "✓",
@@ -13,16 +21,37 @@ return {
 					package_uninstalled = "✗"
 				}
 			}
-		}),
-
+		})
 		require("mason-lspconfig").setup({
+
 			ensure_installed = {
-				"clangd", 
-				"cmake", 
-				"autotools_ls", 
-				"pyright", 
+				"clangd",
+				"pyright",
+				"cmake",
 				"lua_ls",
+				"autotools_ls",
+			},
+			handlers = {
+				function (server_name)
+					require("lspconfig")[server_name].setup({})
+				end,
+
+				["lua_ls"] = function ()
+					local lspconfig = require("lspconfig")
+					lspconfig.lua_ls.setup {
+						settings = {
+							Lua = {
+								diagnostics = {
+									globals = { "vim" }
+								}
+							}
+						}
+					}
+				end,
 			}
 		})
-	end	
+
+	end
 }
+
+
