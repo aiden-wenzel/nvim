dd94a759584c74356f427497a25a1a79ca7c82d3
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-15 15:40:33 -0400

    finished autocomplete

--- lua/aidenwen/plugins/lsp.lua
+++ lua/aidenwen/plugins/lsp.lua
@@ -1,48 +1,52 @@
 return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
-		'neovim/nvim-lspconfig',
-		"hrsh7th/nvim-cmp",
-		'hrsh7th/cmp-nvim-lsp',
-		'hrsh7th/cmp-buffer',
-		'hrsh7th/cmp-path',
-		'hrsh7th/cmp-cmdline',
-		'L3MON4D3/LuaSnip',
-		'saadparwaiz1/cmp_luasnip',
- 		"williamboman/mason.nvim",
+		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
 	},
-	config = function()
-		require("mason").setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗"
-				}
-			}
-		})
-		require("mason-lspconfig").setup({
 
+	config = function()
+		local cmp = require('cmp')
+		local cmp_lsp = require("cmp_nvim_lsp")
+		local capabilities = vim.tbl_deep_extend(
+			"force",
+			{},
+			vim.lsp.protocol.make_client_capabilities(),
+			cmp_lsp.default_capabilities()
+		)
+
+		require("mason").setup()
+		require("mason-lspconfig").setup({
 			ensure_installed = {
 				"clangd",
-				"pyright",
 				"cmake",
-				"lua_ls",
 				"autotools_ls",
+				"pyright",
+				"lua_ls",
 			},
 			handlers = {
-				function (server_name)
-					require("lspconfig")[server_name].setup({})
+				function(server_name)
+					require("lspconfig")[server_name].setup {
+						capabilities = capabilities
+					}
 				end,
 
-				["lua_ls"] = function ()
+				["lua_ls"] = function()
 					local lspconfig = require("lspconfig")
 					lspconfig.lua_ls.setup {
+						capabilities = capabilities,
 						settings = {
 							Lua = {
+								runtime = { version = "Lua 5.1" },
 								diagnostics = {
-									globals = { "vim" }
+									globals = { "vim", "it", "describe", "before_each", "after_each" },
 								}
 							}
 						}
@@ -51,19 +55,20 @@ return {
 			}
 		})
 
-		local cmp = require("cmp")
 		local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
 		cmp.setup({
 			snippet = {
 				expand = function(args)
-					require("luasnip").lsp_expand(args.body)
+					require('luasnip').lsp_expand(args.body)
 				end,
 			},
+
 			window = {
 				completion = cmp.config.window.bordered(),
 				documentation = cmp.config.window.bordered(),
 			},
-			mappings = cmp.mapping.preset.insert({
+			mapping = cmp.mapping.preset.insert({
 				['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 				['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
 				['<C-y>'] = cmp.mapping.confirm({ select = true }),
@@ -71,11 +76,24 @@ return {
 			}),
 			sources = cmp.config.sources({
 				{ name = 'nvim_lsp' },
-               			{ name = 'luasnip' },
+				{ name = 'luasnip' },
+				{ name = 'nvim_lsp_signature_help'},
+			}, {
 				{ name = 'buffer' },
-			}),
+			})
 		})
 
-
+		vim.diagnostic.config({
+			float = {
+				focusable = false,
+				style = "minimal",
+				border = "rounded",
+				source = "always",
+				header = "",
+				prefix = "",
+			},
+		})
 	end
 }
+
+
