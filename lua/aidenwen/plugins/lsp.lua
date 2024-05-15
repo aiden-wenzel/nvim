1a088b3a42eed15d859ceffd87725c7f0a21a626
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-15 09:07:41 -0400

    nvim mason-lspconfig

--- lua/aidenwen/plugins/lsp.lua
+++ lua/aidenwen/plugins/lsp.lua
@@ -1,16 +1,28 @@
 return {
 	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
 
 	config = function() 
 		require("mason").setup({
 
-		ui = {
-			icons = {
-				package_installed = "✓",
-				package_pending = "➜",
-				package_uninstalled = "✗"
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗"
+				}
 			}
-		}
-	})
+		}),
+
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"clangd", 
+				"cmake", 
+				"autotools_ls", 
+				"pyright", 
+				"lua_ls",
+			}
+		})
 	end	
 }
