366255572429fa2d510bb31076f4b33343b0ce54
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-06-04 13:01:09 -0400

    disabled latex in treesitter

--- lua/aidenwen/plugins/treesitter.lua
+++ lua/aidenwen/plugins/treesitter.lua
@@ -1,10 +1,11 @@
 return {
-	'nvim-treesitter/nvim-treesitter', 
+	'nvim-treesitter/nvim-treesitter',
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = {"c", "cpp", "python", "lua"},
 			auto_install = true,
 			highlight = {
+                disable = {"latex"},
 				enable = true,
 				additional_vim_regex_highlighting = false
 			}
