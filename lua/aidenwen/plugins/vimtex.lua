47ccd0bd93727afa23ef469224278aa7f08a414b
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-06-04 13:01:49 -0400

    set lazy load to false and enabled default mappings

--- lua/aidenwen/plugins/vimtex.lua
+++ lua/aidenwen/plugins/vimtex.lua
@@ -1,7 +1,9 @@
 return {
     "lervag/vimtex",
+    lazy = false,
     config = function()
        vim.cmd("filetype plugin on")
        vim.cmd("set encoding=utf8")
+       vim.cmd("let g:vimtex_mappings_enabled=1")
     end
 }
