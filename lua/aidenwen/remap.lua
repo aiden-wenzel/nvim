fd8393144ae069867c8ea713290058ef7e3a972f
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-06-14 13:51:59 -0400

    added split screen keybinds

--- lua/aidenwen/remap.lua
+++ lua/aidenwen/remap.lua
@@ -6,3 +6,7 @@ vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+-- split screen
+vim.keymap.set('n', '<leader>%', ':vsplit<CR>')
+vim.keymap.set('n', '<leader>\"', ':split<CR>')
