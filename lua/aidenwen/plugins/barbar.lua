4ce75279d6ad172f59eb2b84709d2cb62d5b4c50
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-07-01 18:13:32 -0400

    barbar tabs

---
+++ lua/aidenwen/plugins/barbar.lua
@@ -0,0 +1,18 @@
+return {
+    "romgrk/barbar.nvim",
+
+    dependencies = {
+        "lewis6991/gitsigns.nvim",
+        "nvim-tree/nvim-web-devicons",
+    },
+
+    config = function()
+        vim.g.barbar_auto_setup = false
+        local map = vim.api.nvim_set_keymap
+        local opts = {noremap = true, silent = true}
+
+        map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+        map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+        map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+    end
+}
--- lua/aidenwen/style.lua
+++ lua/aidenwen/style.lua
@@ -9,3 +9,17 @@ set.shiftwidth = 4
 set.expandtab = true
 
 set.smartindent = true
+
+set.swapfile = false
+set.backup = false
+set.undodir = os.getenv("HOME") .. "/.vim/undodir"
+set.undofile = true
+
+set.hlsearch = false
+set.incsearch = true
+
+set.termguicolors = true
+
+set.scrolloff = 8
+
+set.updatetime = 50
