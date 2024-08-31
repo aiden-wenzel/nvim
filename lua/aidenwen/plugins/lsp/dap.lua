ddb5d0c545d87537fd0c44a058e3c43333dee426
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-08-31 10:15:58 -0400

    Add C and Dap UI debugging

--- lua/aidenwen/plugins/lsp/dap.lua
+++ lua/aidenwen/plugins/lsp/dap.lua
@@ -9,6 +9,7 @@ return {
 	},
 	config = function()
 		local dap = require("dap")
+		local dapui = require("dapui")
 
 		vim.keymap.set('n', '<F5>', function() dap.continue() end)
 		vim.keymap.set('n', '<F10>', function() dap.step_over() end)
@@ -17,6 +18,10 @@ return {
 		vim.keymap.set('n', '<Leader>b', function() dap.toggle_breakpoint() end)
 		vim.keymap.set('n', '<Leader>dr', function() dap.repl.toggle() end)
 		vim.keymap.set('n', '<Leader>dl', function() dap.run_last() end)
+
+		dapui.setup()
+		vim.keymap.set('n', '<Leader>duo', function() dapui.open() end)
+		vim.keymap.set('n', '<Leader>duc', function() dapui.close() end)
 		vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
 			require('dap.ui.widgets').hover()
 		end)
@@ -24,7 +29,7 @@ return {
 		dap.adapters.gdb = {
 			type = "executable",
 			command = "gdb",
-			args = { "-i", "dap" }
+			args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
 		}
 
 		dap.configurations.cpp = {
@@ -41,6 +46,41 @@ return {
 			},
 		}
 
+		dap.configurations.c = {
+			{
+				name = "Launch",
+				type = "gdb",
+				request = "launch",
+				program = function()
+					return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+				end,
+				cwd = "${workspaceFolder}",
+				stopAtBeginningOfMainSubprogram = true,
+			},
+			{
+				name = "Select and attach to process",
+				type = "gdb",
+				request = "attach",
+				program = function()
+					return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+				end,
+				pid = function()
+					local name = vim.fn.input('Executable name (filter): ')
+					return require("dap.utils").pick_process({ filter = name })
+				end,
+				cwd = '${workspaceFolder}'
+			},
+			{
+				name = 'Attach to gdbserver :1234',
+				type = 'gdb',
+				request = 'attach',
+				target = 'localhost:1234',
+				program = function()
+					return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+				end,
+				cwd = '${workspaceFolder}'
+			},
+		}
 		require('dap-python').setup('.venv/bin/python')
 	end
 }
