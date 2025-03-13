a51017187bf7e351941954735c59820fe80337ca
Aiden Wenzel <wenzel.aiden@outlook.com>
2025-03-13 14:43:05 -0400

    ;alksdjf;alsdf

--- lua/aidenwen/plugins/lsp/cmp.lua
+++ lua/aidenwen/plugins/lsp/cmp.lua
@@ -53,10 +53,10 @@ return {
 		}),
 
 			sources = cmp.config.sources({
-				-- {name = 'nvim_lsp'},
-				-- {name = 'luasnip'},
-				-- {name = 'nvim_lsp_signature_help'},
-				{name = 'buffer'}
+				{name = 'nvim_lsp'},
+				{name = 'luasnip'},
+				{name = 'nvim_lsp_signature_help'},
+				-- {name = 'buffer'}
 			})
 		})
 		vim.diagnostic.config({
