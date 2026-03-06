59743829ecf211c024c4cc727423f35f6ab845bd
Aiden Wenzel <wenzel.aiden@outlook.com>
2026-03-06 16:53:13 -0500

    prevent warnings for vim global variable

--- lua/aidenwen/plugins/lsp/lsp.lua
+++ lua/aidenwen/plugins/lsp/lsp.lua
@@ -8,6 +8,19 @@ return {
 	config = function()
 		require("mason").setup()
 		vim.lsp.enable("pyright")
+
 		vim.lsp.enable("lua_ls")
+		vim.lsp.config("lua_ls", {
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = {"vim"}
+					}
+				}
+			}
+		})
+
+		vim.lsp.enable("clangd")
+
 	end
 }
