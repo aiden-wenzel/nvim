3a44e3bf012b6434b548b9ce5befeaf411836d45
Aiden Wenzel <wenzel.aiden@outlook.com>
2026-03-06 16:12:50 -0500

    remove plugins I don't use

--- lua/aidenwen/plugins/source-control/undotree.lua
+++
@@ -1,9 +0,0 @@
--- Purpose: Provide neovim with a history of saved changes.
-
-return {
-	"mbbill/undotree",
-
-	config = function ()
-		vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle)
-	end
-}
